8bce83880cba2b82089737ff4b464002a7dd9100
Pedro T. Specian <pts.mdascruzes@gmail.com>
2016-11-07 00:01:35 -0200

    Ajustes na tela de graduacao

--- src/BD/detalheAluno.php
+++ src/BD/detalheAluno.php
@@ -3,36 +3,39 @@
 	include "queries.php";
 ?>
 <main>
-    <h1>Projetos de extensão do aluno:</h1>
-    <br>
-    <h1>Projetos de pesquisa do aluno</h1>
-    <br>
-    <h1>Disciplinas cursadas pelo aluno</h1>
-    <table class="table">
-		<thead>
-		    <tr>
-    		    <th>Código disciplina</th>
-    		    <th>Nome disciplina</th>
-    		    <th>Nota do aluno</th>
-    		    <tH>Frequencia do aluno</tH>
-		   	</tr>
-    	</thead>
-	    <tbody>
-	        
-	    	<?php
-	    		$alunos = consultaHistoricoAluno($_GET['idAluno']);
-	    		while ($dados = mysqli_fetch_array($alunos)) {
-	    			echo "<tr>";
-	    			echo "<td>".$dados['Codigo']."</td>";
-	    			echo "<td>".$dados['nome']."</td>";
-	    			echo "<td>".$dados['Notas']."</td>";
-	    			echo "<td>".$dados['Frequencia']."</td>";
-	    			echo "</tr>";
-	    		}
-	    	?>
-	    	
-		</tbody>
+	<div class="container">
+		<h2>Aluno: <?php echo $_GET['nomeAluno'];?></h2>
+	    <h3>Projetos de extensão do aluno:</h3>
+
+	    <h3>Projetos de pesquisa do aluno</h3>
+
+	    <h3>Disciplinas cursadas pelo aluno</h3>
+	    <table class="table">
+			<thead>
+			    <tr>
+	    		    <th>Código disciplina</th>
+	    		    <th>Nome disciplina</th>
+	    		    <th>Nota do aluno</th>
+	    		    <th>Frequencia do aluno</th>
+			   	</tr>
+	    	</thead>
+		    <tbody>
+		        
+		    	<?php
+		    		$alunos = consultaHistoricoAluno($_GET['idAluno']);
+		    		while ($dados = mysqli_fetch_array($alunos)) {
+		    			echo "<tr>";
+		    			echo "<td>".$dados['Codigo']."</td>";
+		    			echo "<td>".$dados['nome']."</td>";
+		    			echo "<td>".$dados['Notas']."</td>";
+		    			echo "<td>".$dados['Frequencia']."</td>";
+		    			echo "</tr>";
+		    		}
+		    	?>
+		    	
+			</tbody>
 		</table>
+	</div>
 </main>
 <?php 
 	include "footer.html"; 

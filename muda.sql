fd7997ed869096163d13b5db4fbec542b3e2e8a8
Lucas Delboni <lucas_cd460@hotmail.com>
2016-11-19 18:25:52 +0000

    sql para colocar auto increment no bd

---
+++ muda.sql
@@ -0,0 +1,10 @@
+-- mudanças pra auto increment
+
+ALTER TABLE  `Usuario` CHANGE  `ID_Usuario`  `ID_Usuario` INT( 11 ) NOT NULL AUTO_INCREMENT ;
+ALTER TABLE  `Unidade` CHANGE  `ID_Unidade`  `ID_Unidade` INT( 11 ) NOT NULL AUTO_INCREMENT ;
+ALTER TABLE  `Projeto` CHANGE  `ID_Projeto`  `ID_Projeto` BIGINT( 20 ) NOT NULL AUTO_INCREMENT ;
+ALTER TABLE  `Ocorrencias` CHANGE  `ID_Ocorrencia`  `ID_Ocorrencia` INT( 11 ) NOT NULL AUTO_INCREMENT ;
+ALTER TABLE  `Financiador` CHANGE  `ID_Financiador`  `ID_Financiador` INT( 11 ) NOT NULL AUTO_INCREMENT ;
+ALTER TABLE  `Emprestimos` CHANGE  `Id_Emprestimo`  `Id_Emprestimo` BIGINT( 20 ) NOT NULL AUTO_INCREMENT ;
+ALTER TABLE  `Disciplinas` CHANGE  `ID_Disciplina`  `ID_Disciplina` INT( 11 ) NOT NULL AUTO_INCREMENT ;
+ALTER TABLE  `Cursos` CHANGE  `Codigo`  `Codigo` BIGINT( 20 ) NOT NULL AUTO_INCREMENT ;

ccbcd58eb8c30d4cd44811613305b5c109f81016
Pedro Toupitzen <pedrotoupitzen@luego.com.br>
2017-01-09 16:05:10 -0200

    ajuste nas iniciais das tabelas do muda.sql

--- muda.sql
+++ muda.sql
@@ -1,12 +1,12 @@
 -- mudanças pra auto increment
 
-ALTER TABLE  `Usuario` CHANGE  `ID_Usuario`  `ID_Usuario` INT( 11 ) NOT NULL AUTO_INCREMENT ;
-ALTER TABLE  `Unidade` CHANGE  `ID_Unidade`  `ID_Unidade` INT( 11 ) NOT NULL AUTO_INCREMENT ;
-ALTER TABLE  `Projeto` CHANGE  `ID_Projeto`  `ID_Projeto` BIGINT( 20 ) NOT NULL AUTO_INCREMENT ;
-ALTER TABLE  `Ocorrencias` CHANGE  `ID_Ocorrencia`  `ID_Ocorrencia` INT( 11 ) NOT NULL AUTO_INCREMENT ;
-ALTER TABLE  `Financiador` CHANGE  `ID_Financiador`  `ID_Financiador` INT( 11 ) NOT NULL AUTO_INCREMENT ;
-ALTER TABLE  `Emprestimos` CHANGE  `Id_Emprestimo`  `Id_Emprestimo` BIGINT( 20 ) NOT NULL AUTO_INCREMENT ;
-ALTER TABLE  `Disciplinas` CHANGE  `ID_Disciplina`  `ID_Disciplina` INT( 11 ) NOT NULL AUTO_INCREMENT ;
-ALTER TABLE  `Cursos` CHANGE  `Codigo`  `Codigo` BIGINT( 20 ) NOT NULL AUTO_INCREMENT ;
-ALTER TABLE  `Bens` CHANGE  `ID_bem`  `ID_bem` INT( 11 ) NOT NULL AUTO_INCREMENT ;
+ALTER TABLE  `usuario` CHANGE  `ID_Usuario`  `ID_Usuario` INT( 11 ) NOT NULL AUTO_INCREMENT ;
+ALTER TABLE  `unidade` CHANGE  `ID_Unidade`  `ID_Unidade` INT( 11 ) NOT NULL AUTO_INCREMENT ;
+ALTER TABLE  `projeto` CHANGE  `ID_Projeto`  `ID_Projeto` BIGINT( 20 ) NOT NULL AUTO_INCREMENT ;
+ALTER TABLE  `ocorrencias` CHANGE  `ID_Ocorrencia`  `ID_Ocorrencia` INT( 11 ) NOT NULL AUTO_INCREMENT ;
+ALTER TABLE  `financiador` CHANGE  `ID_Financiador`  `ID_Financiador` INT( 11 ) NOT NULL AUTO_INCREMENT ;
+ALTER TABLE  `emprestimos` CHANGE  `Id_Emprestimo`  `Id_Emprestimo` BIGINT( 20 ) NOT NULL AUTO_INCREMENT ;
+ALTER TABLE  `disciplinas` CHANGE  `ID_Disciplina`  `ID_Disciplina` INT( 11 ) NOT NULL AUTO_INCREMENT ;
+ALTER TABLE  `cursos` CHANGE  `Codigo`  `Codigo` BIGINT( 20 ) NOT NULL AUTO_INCREMENT ;
+ALTER TABLE  `bens` CHANGE  `ID_bem`  `ID_bem` INT( 11 ) NOT NULL AUTO_INCREMENT ;
 
